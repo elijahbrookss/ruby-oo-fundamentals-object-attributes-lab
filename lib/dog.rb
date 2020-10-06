f829242a3d3b89e26b7ff3b8adc7a3c16573063a
elijahbrookss <elijahcbrooks@gmail.com>
2020-10-06 16:13:16 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+
+    def initialize()
+        @name
+        @breed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed=(breed_name)
+        @breed = breed_name
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+end
+
+
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+
+class Person
+
+    def initialize()
+        @name;
+        @job;
+    end
+
+    def name
+        @name
+    end
+    def job
+        @job
+    end
+
+    def name=(name)
+        @name = name;
+    end
+
+    def job=(job_name)
+        @job = job_name
+    end
+end
